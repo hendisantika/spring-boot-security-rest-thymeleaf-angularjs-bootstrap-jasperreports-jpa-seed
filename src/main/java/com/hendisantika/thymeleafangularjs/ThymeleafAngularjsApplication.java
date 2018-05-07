124611fc44ec6b6339fc74088190a2f5b19bef2c
Hendi Santika <hendisantika@gmail.com>
2018-05-08 06:33:41 +0700

    #2 - Initial Commit

---
+++ src/main/java/com/hendisantika/thymeleafangularjs/ThymeleafAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.thymeleafangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafAngularjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafAngularjsApplication.class, args);
+    }
+}
