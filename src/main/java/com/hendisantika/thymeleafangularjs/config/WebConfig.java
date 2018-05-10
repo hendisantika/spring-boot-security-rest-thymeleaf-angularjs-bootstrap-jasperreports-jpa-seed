7dab38b1caee3a29128c345ca2d356f737f1d6e0
Hendi Santika <hendisantika@gmail.com>
2018-05-11 05:56:25 +0700

    #12 - Add Web Config

---
+++ src/main/java/com/hendisantika/thymeleafangularjs/config/WebConfig.java
@@ -0,0 +1,70 @@
+package com.hendisantika.thymeleafangularjs.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : thymeleaf-angularjs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/05/18
+ * Time: 05.54
+ * To change this template use File | Settings | File Templates.
+ */
+
+@EnableAutoConfiguration
+@ComponentScan("com.hendisantika.thymeleafangularjs")
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Configuration
+    protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("demo").password("demo").roles("demo");
+        }
+    }
+
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeRequests().antMatchers("/report").permitAll().antMatchers("/jasper/**").permitAll().antMatchers("/webjars/**")
+                    .permitAll().antMatchers("/ng-templates/**").permitAll().antMatchers("/rest/**").permitAll().antMatchers("/js/**").permitAll().anyRequest()
+                    .fullyAuthenticated().and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll().and().logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().logoutSuccessUrl("/login?logout");
+
+        }
+    }
+}
